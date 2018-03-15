9b4577921d05aaf81e9017b6e0edd84f62a0199e
towakey <kyosgame@gmail.com>
2018-03-16 00:05:39 +0900

    txt delted

--- rubeep.rb
+++ rubeep.rb
@@ -3,49 +3,49 @@ require 'Win32API'
 class Rubeep
     def initialize
         @bp=Win32API.new('kernel32',"Beep",%w(i i),'i')
+        @basic_time=1000
+    end
+    def load(fn)
+        @arr=[]
+        File.open(fn,mode='rt:utf-8'){|f|
+            @arr=f.readlines
+        }
+    end
+    def play
+        @arr.each{|melody|
+            self.beep(melody.chomp!,@basic_time)
+        }
     end
     def beep(scale,time)
         case scale
-        when 'lf' then
-            hz=294
-        when 'lg' then
-            hz=330
-        when 'la' then
-            hz=370
-        when 'lb' then
-            hz=415
-        when 'c' then   #ド
-            hz=440
-        when 'd' then   #レ
-            hz=494
-        when 'e' then   #ミ
-            hz=554
-        when 'f' then   #ファ
-            hz=587
-        when 'g' then   #ソ
-            hz=659
-        when 'a' then   #ラ
-            hz=740
-        when 'b' then   #シ
-            hz=830
-        when 'hc' then  #ド
-            hz=880
-        when 'hd' then
-            hz=988
-        when 'he' then
-            hz=1109
-        when 'hf' then
-            hz=1175
-        when 'hg' then
-            hz=1319
-        when 'ha' then
-            hz=1480
-        when 'hb' then
-            hz=1661
-        else
+        when 'lf' then hz=294
+        when 'lg' then hz=330
+        when 'la' then hz=370
+        when 'lb' then hz=415
+        when 'c' then hz=440    #ド
+        when 'd' then hz=494    #レ
+        when 'e' then hz=554    #ミ
+        when 'f' then hz=587    #ファ
+        when 'g' then hz=659    #ソ
+        when 'a' then hz=740    #ラ
+        when 'b' then hz=830    #シ
+        when 'hc' then hz=880   #ド
+        when 'hd' then hz=988
+        when 'he' then hz=1109
+        when 'hf' then hz=1175
+        when 'hg' then hz=1319
+        when 'ha' then hz=1480
+        when 'hb' then hz=1661
+        else hz=0
         end
         @bp.call(hz,time)
     end
+end
+
+begin
+    rubeep=Rubeep.new
+    rubeep.load('furura.txt')
+    rubeep.play
 end
 
 =begin
