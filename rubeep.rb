6c64dcc5a867c70b21645784af619aefbc5a4d9b
towakey <kyosgame@gmail.com>
2018-04-14 01:52:40 +0900

    音階の計算方法変更 同時に音符の記述方法変更

--- rubeep.rb
+++ rubeep.rb
@@ -11,45 +11,32 @@ class Rubeep
         File.open(fn,mode='rt:utf-8'){|f|
             @arr=f.readlines
         }
-    end
-    def play
-        @arr.each{|melody|
-            self.beep(melody.chomp!,@basic_time)
+        @arr2=[]
+        @arr.each{|tmp|
+            @arr2<< tmp.chomp!.split(":")
         }
     end
-    def beep(scale,time)
-        case scale
-        when 'lf' then hz=-16   #ファ
-        when 'lf#' then hz=-15  #ファ#
-        when 'lg' then hz=-14   #ソ
-        when 'lg#' then hz=-13  #ソ#
-        when 'la' then hz=-12   #ラ
-        when 'la#' then hz=-11  #ラ#
-        when 'lb' then hz=-10   #シ
-        when 'c' then hz=-9     #ド
-        when 'c#' then hz=-8    #ド#
-        when 'd' then hz=-7     #レ
-        when 'd#' then hz=-6    #レ#
-        when 'e' then hz=-5     #ミ
-        when 'f' then hz=-4     #ファ
-        when 'f#' then hz=-3    #ファ#
-        when 'g' then hz=-2     #ソ
-        when 'g#' then hz=-1    #ソ#
-        when 'a' then hz=0      #ラ
-        when 'a#' then hz=1     #ラ#
-        when 'b' then hz=2      #シ
-        when 'hc' then hz=3     #ド
-        when 'hc#' then hz=4    #ド#
-        when 'hd' then hz=5     #レ
-        when 'hd#' then hz=6    #レ#
-        when 'he' then hz=7     #ミ
-        when 'hf' then hz=8     #ファ
-        when 'hf#' then hz=9    #ファ#
-        when 'hg' then hz=10    #ソ
-        when 'hg#' then hz=11   #ソ#
-        when 'ha' then hz=12    #ラ
-        when 'ha#' then hz=13   #ラ#
-        when 'hb' then hz=14    #シ
+    def play
+        @arr2.each{|melody,scale|
+            p melody
+            p scale
+            self.beep(melody,scale,@basic_time)
+        }
+    end
+    def beep(melody,scale,time)
+        case melody
+        when 'c' then hz=-9+(scale.to_i*12)     #ド
+        when 'c#' then hz=-8+(scale.to_i*12)    #ド#
+        when 'd' then hz=-7+(scale.to_i*12)     #レ
+        when 'd#' then hz=-6+(scale.to_i*12)    #レ#
+        when 'e' then hz=-5+(scale.to_i*12)     #ミ
+        when 'f' then hz=-4+(scale.to_i*12)     #ファ
+        when 'f#' then hz=-3+(scale.to_i*12)    #ファ#
+        when 'g' then hz=-2+(scale.to_i*12)     #ソ
+        when 'g#' then hz=-1+(scale.to_i*12)    #ソ#
+        when 'a' then hz=0+(scale.to_i*12)      #ラ
+        when 'a#' then hz=1+(scale.to_i*12)     #ラ#
+        when 'b' then hz=2+(scale.to_i*12)      #シ
         else hz=0
         end
         @bp.call(440*(2**(hz.to_f/12)),time)
